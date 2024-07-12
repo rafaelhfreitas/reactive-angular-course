a60f2cd993b2522eaa430da7d6200ec2237a6b4c
Rafael Freitas <oraffaudi@gmail.com>
2024-07-12 09:45:07 -0300

    fix backend to dosent send error in PUT request

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -5,10 +5,11 @@ import {setTimeout} from 'timers';
 
 export function saveCourse(req: Request, res: Response) {
 
-  
+  /*
   console.log("ERROR saving course!");
   res.sendStatus(500);
   return;
+  */
 
   
 

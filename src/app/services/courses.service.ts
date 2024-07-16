018f887baf98bada5bbba0510e78767dd8e7d618
Rafael Freitas <oraffaudi@gmail.com>
2024-07-16 11:11:07 -0300

    Consolidation Exercise - Implementing the Course Screen in Reactive Style

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -23,6 +23,14 @@ export class CourseService {
     }
 
 
+    loadCourseById(courseId: number): Observable<Course> {
+        return this.http.get<Course>(`/api/courses/${courseId}`)
+            .pipe(
+                shareReplay()
+            )
+    }
+
+
     saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
         return this.http.put(`/api/courses/${courseId}`, changes)
                 .pipe(

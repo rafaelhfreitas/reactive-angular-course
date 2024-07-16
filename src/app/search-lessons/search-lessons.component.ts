d859a3a70d48dcb7c3811597217472a83cc1acff
Rafael Freitas <oraffaudi@gmail.com>
2024-07-16 10:04:55 -0300

    Angular Master Detail Implementation - The Master Table

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CourseService } from '../services/courses.service';
 
 
 @Component({
@@ -24,13 +25,22 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+
+  searchResults$: Observable<Lesson[]>;
+
+  constructor(private coursesService: CourseService) {
 
 
   }
 
   ngOnInit() {
 
+
+  }
+
+  onSearch(search: string) {
+
+    this.searchResults$ = this.coursesService.searchLessons(search);
 
   }
 

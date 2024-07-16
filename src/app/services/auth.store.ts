76452dd696277277a3a86bb3ac5a42756502c694
Rafael Freitas <oraffaudi@gmail.com>
2024-07-16 09:04:31 -0300

    Authentication State Management - Section Introduction

---
+++ src/app/services/auth.store.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../model/user";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthStore {
+
+    user$: Observable<User>;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
+
+    login(email: string, password: string): Observable<User> {
+        return undefined;
+
+    }
+
+
+
+    logout(){
+        
+    }
+
+
+}
